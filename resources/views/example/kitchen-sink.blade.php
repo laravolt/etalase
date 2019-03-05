837fe24ff92429e65b87553997d495e1b0eb8875
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2019-03-05 20:13:49 +0700

    Add more semantic-form sample

--- resources/views/example/kitchen-sink.blade.php
+++ resources/views/example/kitchen-sink.blade.php
@@ -7,51 +7,65 @@
 
     <h3 class="ui header">Regular Form</h3>
     {!! form()->open('') !!}
-    {!! form()->text('one wide')->label('Name')->fieldWidth(1) !!}
-    {!! form()->text('two wide')->label('Password')->fieldWidth(2) !!}
-    {!! form()->text('three wide')->label('Password')->fieldWidth(3) !!}
-    {!! form()->text('four wide')->label('Password')->fieldWidth(4) !!}
-    {!! form()->text('five wide')->label('Password')->fieldWidth(5) !!}
-    {!! form()->text('six wide')->label('Password')->fieldWidth(6) !!}
-    {!! form()->text('seven wide')->label('Password')->fieldWidth(7) !!}
-    {!! form()->text('eight wide')->label('Password')->fieldWidth(8) !!}
-    {!! form()->text('nine wide')->label('Password')->fieldWidth(9) !!}
-    {!! form()->text('ten wide')->label('Password')->fieldWidth(10) !!}
-    {!! form()->text('eleven wide')->label('Password')->fieldWidth(11) !!}
-    {!! form()->text('twelve wide')->label('Password')->fieldWidth(12) !!}
-    {!! form()->text('thirteen wide')->label('Password')->fieldWidth(13) !!}
-    {!! form()->text('fourteen wide')->label('Password')->fieldWidth(14) !!}
-    {!! form()->text('fiveteen wide')->label('Password')->fieldWidth(15) !!}
-    {!! form()->text('sixteen wide')->label('Password')->fieldWidth(16) !!}
+    {!! form()->text('')->label('one wide')->fieldWidth(1) !!}
+    {!! form()->text('')->label('two wide')->fieldWidth(2) !!}
+    {!! form()->text('')->label('three wide')->fieldWidth(3) !!}
+    {!! form()->text('')->label('four wide')->fieldWidth(4) !!}
+    {!! form()->text('')->label('five wide')->fieldWidth(5) !!}
+    {!! form()->text('')->label('six wide')->fieldWidth(6) !!}
+    {!! form()->text('')->label('seven wide')->fieldWidth(7) !!}
+    {!! form()->text('')->label('eight wide')->fieldWidth(8) !!}
+    {!! form()->text('')->label('nine wide')->fieldWidth(9) !!}
+    {!! form()->text('')->label('ten wide')->fieldWidth(10) !!}
+    {!! form()->text('')->label('eleven wide')->fieldWidth(11) !!}
+    {!! form()->text('')->label('twelve wide')->fieldWidth(12) !!}
+    {!! form()->text('')->label('thirteen wide')->fieldWidth(13) !!}
+    {!! form()->text('')->label('fourteen wide')->fieldWidth(14) !!}
+    {!! form()->text('')->label('fifteen wide')->fieldWidth(15) !!}
+    {!! form()->text('')->label('sixteen wide')->fieldWidth(16) !!}
     {!! form()->action(form()->submit('Submit')) !!}
     {!! form()->close() !!}
 
     <h3 class="ui header">Horizontal Form</h3>
     {!! form()->open('')->horizontal() !!}
-    {!! form()->text('one wide')->label('Name')->fieldWidth(1) !!}
-    {!! form()->text('two wide')->label('Password')->fieldWidth(2) !!}
-    {!! form()->text('three wide')->label('Password')->fieldWidth(3) !!}
-    {!! form()->text('four wide')->label('Password')->fieldWidth(4) !!}
-    {!! form()->text('five wide')->label('Password')->fieldWidth(5) !!}
-    {!! form()->text('six wide')->label('Password')->fieldWidth(6) !!}
-    {!! form()->text('seven wide')->label('Password')->fieldWidth(7) !!}
-    {!! form()->text('eight wide')->label('Password')->fieldWidth(8) !!}
-    {!! form()->text('nine wide')->label('Password')->fieldWidth(9) !!}
-    {!! form()->text('ten wide')->label('Password')->fieldWidth(10) !!}
-    {!! form()->text('eleven wide')->label('Password')->fieldWidth(11) !!}
-    {!! form()->text('twelve wide')->label('Password')->fieldWidth(12) !!}
-    {!! form()->text('thirteen wide')->label('Password')->fieldWidth(13) !!}
-    {!! form()->text('fourteen wide')->label('Password')->fieldWidth(14) !!}
-    {!! form()->text('fiveteen wide')->label('Password')->fieldWidth(15) !!}
-    {!! form()->text('sixteen wide')->label('Password')->fieldWidth(16) !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
+    {!! form()->text('')->label('Label') !!}
     {!! form()->action(form()->submit('Submit')) !!}
     {!! form()->close() !!}
 
-    <h3 class="ui header">Horizontal Form With Hint</h3>
+    <h3 class="ui header">Horizontal Form With Custom Width</h3>
     {!! form()->open('')->horizontal() !!}
-    {!! form()->text('one wide')->label('Name')->hint('lorem ipsum')->fieldWidth(1) !!}
-    {!! form()->text('eight wide')->label('Password')->hint('lorem ipsum')->fieldWidth(8) !!}
-    {!! form()->text('sixteen wide')->label('Password')->hint('lorem ipsum')->fieldWidth(16) !!}
+    {!! form()->text('')->label('one wide')->fieldWidth(1) !!}
+    {!! form()->text('')->label('two wide')->fieldWidth(2) !!}
+    {!! form()->text('')->label('three wide')->fieldWidth(3) !!}
+    {!! form()->text('')->label('four wide')->fieldWidth(4) !!}
+    {!! form()->text('')->label('five wide')->fieldWidth(5) !!}
+    {!! form()->text('')->label('six wide')->fieldWidth(6) !!}
+    {!! form()->text('')->label('seven wide')->fieldWidth(7) !!}
+    {!! form()->text('')->label('eight wide')->fieldWidth(8) !!}
+    {!! form()->text('')->label('nine wide')->fieldWidth(9) !!}
+    {!! form()->text('')->label('ten wide')->fieldWidth(10) !!}
+    {!! form()->text('')->label('eleven wide')->fieldWidth(11) !!}
+    {!! form()->text('')->label('twelve wide')->fieldWidth(12) !!}
+    {!! form()->text('')->label('thirteen wide')->fieldWidth(13) !!}
+    {!! form()->text('')->label('fourteen wide')->fieldWidth(14) !!}
+    {!! form()->text('')->label('fifteen wide')->fieldWidth(15) !!}
+    {!! form()->text('')->label('sixteen wide')->fieldWidth(16) !!}
     {!! form()->action(form()->submit('Submit')) !!}
     {!! form()->close() !!}
+
 @endsection
